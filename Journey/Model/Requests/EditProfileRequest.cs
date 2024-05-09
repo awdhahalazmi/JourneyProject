131ebad11a2c0cab30580ae589fae901b1173954
awdah <awdah@LAPTOP-DH3O97S3>
2024-05-09 16:17:47 +0300

    this is the project

--- Journey/Model/Requests/EditProfileRequest.cs
+++ Journey/Model/Requests/EditProfileRequest.cs
@@ -3,7 +3,7 @@
     public class EditProfileRequest
     {
         public string Name { get; set; }
-        public string Email { get; set; }
+       // public string Email { get; set; }
         public IFormFile Image { get; set; }
     }
 
